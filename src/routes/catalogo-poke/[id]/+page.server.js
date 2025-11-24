f8fae8f19e793f99cbcf0e13868b008bf5289a47
Gigi <gianlucadeambrosilavoro@gmail.com>
2025-11-24 01:18:29 -0300

    Agrego script de prueba para cargar poke por ID.

--- src/routes/catalogo-poke/[id]/+page.server.js
+++ src/routes/catalogo-poke/[id]/+page.server.js
@@ -1,2 +1,19 @@
 /* Aquí entrará el el endpoint BUSCAR POKÉMON POR ID. */
+/*
+export async function load(id) {
+    let url = new URL(`http://localhost:8000/api/pokemon/${id}`)
 
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        error(`Response status: ${response.status}`);
+        return;
+    }
+
+    let pokemon = await response.json();
+
+    return {
+        pokemon
+    };
+}
+*/
